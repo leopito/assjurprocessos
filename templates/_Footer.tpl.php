206cd7246b6d56c54adc0629554a8c9e0e6cfa4a
Informatica <Informatica@Desktop-0982>
2019-07-12 17:49:27 -0300

    Modificando uma letra.

--- templates/_Footer.tpl.php
+++ templates/_Footer.tpl.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<hr>
 		<footer>
-			<p class="muted"><img src="./images/logo-iterj-min.png" width="20px" height="20px" alt="logo-iterj"/><small> Instituto de Terras e Cartogr@fia do Estado do Rio de Janeiro - <?php echo date('Y'); ?></small></p>
+			<p class="muted"><img src="./images/logo-iterj-min.png" width="20px" height="20px" alt="logo-iterj"/><small> Instituto de Terras e Cartografia do Estado do Rio de Janeiro - <?php echo date('Y'); ?></small></p>
 		</footer>
 	</div>
 	</body>
